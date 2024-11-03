c492febbb9fac6298ee0527ae0f32dc1ef3dd391
rizkyAhermawan <160604439+rizkyAhermawan@users.noreply.github.com>
2024-11-03 21:21:51 +0700

    tugas implisit intent
    tugas implisit intent

---
+++ app/src/main/java/com/example/myintentapp/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.myintentapp
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btnMoveActivity = findViewById<Button>(R.id.btn_move_activity)
+        btnMoveActivity.setOnClickListener {
+            onClick()
+        }
+        val btnDialNumber = findViewById<Button>(R.id.btn_menelepon)
+        btnDialNumber.setOnClickListener {
+            onDial()
+        }
+    }
+
+    private fun onDial() {
+        val dialNumber = "0821263635758"
+        val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + dialNumber))
+        startActivity(intent)
+    }
+
+    private fun onClick() {
+        val intent = Intent(applicationContext, MoveActivity::class.java)
+        startActivity(intent)
+    }
+}
---
+++ app/src/main/java/com/example/myintentapp/MoveActivity.kt
@@ -0,0 +1,30 @@
+package com.example.myintentapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MoveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_move)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.move)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btnBackActivity = findViewById<Button>(R.id.btn_back)
+        btnBackActivity.setOnClickListener {
+            onTekan()
+        }
+    }
+    private fun onTekan() {
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        startActivity(intent)
+    }
+}
